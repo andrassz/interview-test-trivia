d95f01bac4ebc6c144b85429f2544a714c6c7021
pitiboy <peter.horvath@finnoconsult.at>
2021-10-14 21:58:34 +0200

    initial commit

---
+++ apps/test-trivia/src/app/app.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { Message } from '@finnoconsult-test-trivia/api-interfaces';
+
+export const App = () => {
+  const [m, setMessage] = useState<Message>({ message: '' });
+
+  useEffect(() => {
+    fetch('/api')
+      .then((r) => r.json())
+      .then(setMessage);
+  }, []);
+
+  return (
+    <>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Welcome to test-trivia!</h1>
+        <img
+          width="450"
+          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+          alt="Nx - Smart, Extensible Build Framework"
+        />
+      </div>
+      <div>{m.message}</div>
+    </>
+  );
+};
+
+export default App;
